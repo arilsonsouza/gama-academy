bfd61077554794ddb1a2bd92698e8346fed191b4
Arilson Souza <arilsons61@gmail.com>
2020-05-05 19:34:42 -0300

    Fetching data

--- airbnb-challenge/public/js/main.js
+++ airbnb-challenge/public/js/main.js
@@ -1,3 +1,24 @@
-(function() {
-  
+(async function() {
+
+  let loading = false
+  let error = false
+
+  const fetchData = async () => {
+    const API_URL = 'https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72'
+    let data = []
+    try {
+      loading = true
+      const response = await fetch(API_URL)
+      data = await response.json()        
+    } catch (err) {
+      error = true
+    } finally {
+      loading = false
+    } 
+
+    return data
+  }
+
+  const homes = await fetchData()
+  console.log('DATA: ', homes)
 })()
