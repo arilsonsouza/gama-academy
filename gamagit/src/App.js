dd1a1e52ed426d702f86632ed5db9b14af4f4d0c
Arilson Souza <arilsons61@gmail.com>
2020-05-20 19:13:18 -0300

    Installing and configuring react-router-dom

--- gamagit/src/App.js
+++ gamagit/src/App.js
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const [username, setUsername] = useState('');
