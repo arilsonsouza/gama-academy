be9a41ba34dbcb83b176a8ac92754ec326ed9a5b
Arilson Souza <arilsons61@gmail.com>
2020-05-20 19:17:35 -0300

    Importing directions into the App component

--- gamagit/src/pages/Home/index.js
+++ gamagit/src/pages/Home/index.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function Home () {
+	const [username, setUsername] = useState('');
+  
+  const hanleSearch = async () => {
+    const { data } = await axios.get(`https://api.github.com/users/${username}/repos`);
+    console.log(data)
+  }
+  
 	return (
 		<>
+		 <input 
+      value={username}
+      onChange={e => setUsername(e.target.value)}
+      type='Usuário' 
+      name='username'
+      id='username'
+      className="usuarioInput"/>   
 
+      <button 
+        onClick={hanleSearch}
+        type='button'>
+        Pesquisar
+      </button>  
 		</>
 	);
 }
