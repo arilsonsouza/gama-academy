a8b1c2bb615d9d0b1d6b8670d3fcf6011e8eefc7
Arilson Souza <arilsons61@gmail.com>
2020-06-18 21:31:07 -0300

    Using access modifiers

--- curso-typescript/classes.ts
+++ curso-typescript/classes.ts
@@ -12,3 +12,29 @@ class Data {
 
 const data = new Data(1, 1, 2020);
 console.log(data.dia)
+
+
+class Carro {
+  private velocidadeAtual: number = 0;
+
+  constructor (
+    public marca: string,
+    public modelo: string,
+    private velocidadeMaxima: number = 220
+  ) {}
+
+  private alterarVelocidade (delta: number) {
+    // Validações de aceleracao e frenagem 
+  }
+
+  acelerar () {    
+    this.alterarVelocidade(5)
+  }
+
+  frear () {    
+    this.alterarVelocidade(-5)
+  }
+}
+
+const carro = new Carro('Chevrolet', 'Prisma', 250);
+carro.acelerar()
