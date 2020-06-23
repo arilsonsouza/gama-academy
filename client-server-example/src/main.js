bd6392d3d1fd6ed9c66570b3531991c2e9374e72
Arilson Souza <arilsons61@gmail.com>
2020-06-22 22:08:18 -0300

    GraphQL - Rendering, parameters and redirects

--- client-server-example/src/main.js
+++ client-server-example/src/main.js
@@ -1,4 +1,9 @@
 import { createServer } from 'http';
+import { readFile } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'querystring';
+
+let filePath = '';
 
 const server = createServer((req, res) => {
 	switch(req.url) {
@@ -11,7 +16,53 @@ const server = createServer((req, res) => {
 			}))
 			res.end();
 			break
-		
+		case '/home':
+			filePath = resolve(__dirname, 'pages', 'home.html');
+
+			readFile(filePath,(error, file) => {
+				if (error) {
+					res.writeHead(500, "Can't HTML file")
+					res.end()
+					return
+				}
+
+				res.writeHead(200);
+				res.write(file);
+				res.end();
+			});
+
+			break
+		case '/sign-in':
+			filePath = resolve(__dirname, 'pages', 'sign-in.html');
+
+			readFile(filePath,(error, file) => {
+				if (error) {
+					res.writeHead(500, "Can't HTML file")
+					res.end()
+					return
+				}
+
+				res.writeHead(200);
+				res.write(file);
+				res.end();
+			});
+
+			break
+		case '/authenticate':
+			let data = '';
+			req.on('data', (chunck) => {
+				data += chunck;
+			});
+
+			req.on('end', () => {
+				const parsedData = parse(data);
+				console.log(parsedData);	
+				res.writeHead(301, {
+					Location: '/home'
+				});
+				res.end();
+			})
+			break
 		default :
 			res.writeHead(404, 'Service not found');			
 			res.end();
