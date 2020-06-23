dd2f7e50a150321ae9738c7b6b4d577cf9853ac2
Arilson Souza <arilsons61@gmail.com>
2020-06-22 21:48:25 -0300

    GraphQL - Creating the server

--- client-server-example/src/main.js
+++ client-server-example/src/main.js
@@ -1,1 +1,26 @@
-console.log('Hello');
+import { createServer } from 'http';
+
+const server = createServer((req, res) => {
+	switch(req.url) {
+		case '/status': 
+			res.writeHead(200, {
+				'Content-Type': 'application/json'
+			});
+			res.write(JSON.stringify({
+				status: 'Okay'
+			}))
+			res.end();
+			break
+		
+		default :
+			res.writeHead(404, 'Service not found');			
+			res.end();
+	}
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+	console.log(`Server is litening at http://${HOSTNAME}:${PORT}`)
+});
