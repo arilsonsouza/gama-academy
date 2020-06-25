9fdc8a75a7e4ac6b27fe27b846b2ddc7a2f54143
Arilson Souza <arilsons61@gmail.com>
2020-06-24 21:40:55 -0300

    GraphQL - Changing server to express

---
+++ client-server-example/packages/server/src/main.js
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/status',(_, res) => {
+	res.send({
+		status: 'Okay'
+	})
+});
+
+server.post('/authenticate', (req, res) => {
+	console.log(req.body);
+
+	res.send();
+})
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3333;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
+
+server.listen(PORT, HOSTNAME, () => {
+	console.log(`Server is litening at http://${HOSTNAME}:${PORT}`)
+});
