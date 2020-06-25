b30f3d016a1d5f4402b6ee614840929b4a72375c
Arilson Souza <arilsons61@gmail.com>
2020-06-24 22:30:28 -0300

    GraphQL - Apollo Server

--- client-server-example/packages/server/src/main.js
+++ client-server-example/packages/server/src/main.js
@@ -1,30 +1,67 @@
 import express from 'express';
 import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-const server = express();
+const app = express();
 
-server.use(express.json());
-server.use(cors({
+const server = new ApolloServer({
+	typeDefs: gql`
+		type Client {
+			id: ID!
+			name: String!
+		}
+
+		type Demand {
+			id: ID!
+			name: String!
+			client: Client!
+			deadline: String
+		}
+
+		type Query {
+			demands: [Demand]! 
+		}		
+	`,
+
+	resolvers: {
+		Query: {
+			demands: () => {
+				return []
+			}
+		}
+	}
+
+});
+
+server.applyMiddleware({
+	app,
+	cors: {
+		origin: 'http://localhost:3000'
+	}
+});
+
+/*app.use(express.json());
+app.use(cors({
 	origin: 'http://localhost:3000'
-}));
+}));*/
 
-server.get('/status',(_, res) => {
+/*app.get('/status',(_, res) => {
 	res.send({
 		status: 'Okay'
 	})
 });
 
-server.post('/authenticate', (req, res) => {
+app.post('/authenticate', (req, res) => {
 	console.log(req.body);
 
 	res.send({
 		...req.body
 	});
-})
+})*/
 
 const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3333;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
-server.listen(PORT, HOSTNAME, () => {
+app.listen(PORT, HOSTNAME, () => {
 	console.log(`Server is litening at http://${HOSTNAME}:${PORT}`)
 });
