3c132d0c0feac96b61d1d19f2c33767353dd0b27
Arilson Souza <arilsons61@gmail.com>
2020-06-25 22:19:34 -0300

    GraphQL - Creating GraphQL Queries

--- client-server-example/packages/server/src/graphql/resolvers.js
+++ client-server-example/packages/server/src/graphql/resolvers.js
@@ -1,9 +1,12 @@
 import { resolvers as demandResolvers } from './demand/demand';
+import { resolvers as clientResolvers } from './client/client';
 
 const resolvers = {
 	...demandResolvers,
+	...clientResolvers,
 
 	Query: {
+		...clientResolvers.Query,
 		...demandResolvers.Query
 	}
 }
