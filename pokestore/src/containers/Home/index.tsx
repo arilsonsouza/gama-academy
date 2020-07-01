a4937328e3fbf6c4c24507c62346285cff519c48
Arilson Souza <arilsons61@gmail.com>
2020-07-01 20:20:25 -0300

    Added tailwindcss

--- pokestore/src/containers/Home/index.tsx
+++ pokestore/src/containers/Home/index.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <div>
       <h1>Home page pokestore</h1>
+      <button className="tw-bg-blue-500 hover:tw-bg-blue-700 tw-text-white tw-font-bold tw-py-2 tw-px-4 tw-rounded">
+        Button
+      </button>
     </div>
   );
 }
